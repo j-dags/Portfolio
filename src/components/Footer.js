3fcbf9a3bed810d99f8e3b24b2ca59ec27cff3cf
j-dags <jon.dagdagan@gmail.com>
2021-03-26 10:58:35 -0500

    grid css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,14 @@ import React from 'react'
 function Footer() {
 	return (
 		<section id="footer">
-			<h1>Let's connect!</h1>
-			<p>
-				Feel free to reach out to me if you're looking for a developer, have a
-				question, or just want to connect.
-			</p>
-			<p>email@email.com</p>
+			<div className="section-container">
+				<h1>Let's connect!</h1>
+				<p>
+					Feel free to reach out to me if you're looking for a developer, have a
+					question, or just want to connect.
+				</p>
+				<p>email@email.com</p>
+			</div>
 		</section>
 	)
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,19 +2,26 @@ import React from 'react'
 
 function Projects() {
 	return (
-		<section id="projects">
-			<div>
-				<div className="flexCol">
-					<img src="./InfauxWars.png" alt="Infaux Wars" />
-					<h1>Projects</h1>
-					<h3>Infaux Wars</h3>
-					<p>Fake news detection web app</p>
-				</div>
+		<section>
+			<div className="section-container">
+				<h1>Projects</h1>
+				<div className="grid-container">
+					<div className="flexCol">
+						<img src="./InfauxWars.png" alt="Infaux Wars" />
+						<h3>Infaux Wars</h3>
+						<p>Fake news detection web app</p>
+					</div>
 
-				<div className="flexCol">
-					<img src="./AboveTheBreak.png" alt="Infaux Wars" />
-					<h3>Above The Break</h3>
-					<p>Fantasy basketball stats tracker and visualizer</p>
+					<div className="flexCol">
+						<img src="./AboveTheBreak.png" alt="Infaux Wars" />
+						<h3>Above The Break</h3>
+						<p>Fantasy basketball stats tracker and visualizer</p>
+					</div>
+					<div className="flexCol">
+						<img src="./AboveTheBreak.png" alt="Infaux Wars" />
+						<h3>Medium Article</h3>
+						<p>Fantasy basketball stats tracker and visualizer</p>
+					</div>
 				</div>
 			</div>
 		</section>
